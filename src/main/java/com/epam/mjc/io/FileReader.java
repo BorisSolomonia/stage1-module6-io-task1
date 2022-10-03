4126101266b39f038029304028612c01d644ba30
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2022-10-03 19:22:52 +0400

    Update FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -3,28 +3,33 @@ package com.epam.mjc.io;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
 
 public class FileReader {
-    private Map lineMap = new HashMap();
-    private Profile profile = new Profile();
+    private final Profile profile = new Profile();
     public Profile getDataFromFile(File file) {
+        FileInputStream fileInputStream = null;
         try {
-            FileInputStream fileInputStream = new FileInputStream(file.getAbsolutePath());
+            fileInputStream = new FileInputStream(file.getAbsolutePath());
             int c;
-            String k = "";
+            StringBuilder k = new StringBuilder();
             while ((c = fileInputStream.read()) != -1){
-                k += (String.valueOf((char) c));
+                k.append((char) c);
             }
-            String[] profileList = k.split("[ ,\\n]");
+            String[] profileList = k.toString().split("[ ,\\n]");
             profile.setName(profileList[1]);
             profile.setAge(Integer.valueOf(profileList[3]));
             profile.setEmail(profileList[5]);
             profile.setPhone(Long.valueOf(profileList[7]));
         } catch (IOException e) {
             e.printStackTrace();
+        }finally {
+            if (fileInputStream != null){
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
         return profile;
     }
